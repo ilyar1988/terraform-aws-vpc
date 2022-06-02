5faf8620a8c851af2e9fa01dd210ccfa5beffe31
root <root@Ilya-PC.localdomain>
2022-06-03 01:12:14 +0300

    Changing provider file

---
+++ Vars.tf
@@ -0,0 +1,9 @@
+variable "ilya_vpc_cidr" {}
+# variable "public_subnets" {}
+# variable "private_subnets" {}
+variable "public_1" {}
+variable "public_2" {}
+variable "private_1" {}
+variable "private_2" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
