de59cac3f354e17ae1dac558e8a3a5f6765a781a
Ilya Rahmilevich <ilya@whist.co.il>
2022-06-06 23:24:48 +0300

    Add route table changes

--- variables.tf
+++ variables.tf
@@ -3,10 +3,15 @@ variable "aws_region" {
 }
 
 variable "vpc_cidr" {
-	default = "10.20.0.0/16"
+	default = "172.31.0.0/24"
 }
 
 variable "public_sub" {
 	type = list
 	default = ["172.31.3.0/24","172.31.4.0/24"]
+}
+
+variable "private_sub" {
+	type = list
+	default = ["172.31.103.0/24","172.31.104.0/24"]
 }
--- vpc.tf
+++ vpc.tf
@@ -5,14 +5,6 @@ resource "aws_vpc" "terra_vpc" {
   }
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "terra_igw" {
-  vpc_id = aws_vpc.terra_vpc.id
-  tags = {
-    Name = "ilya-vpc"
-  }
-}
-
 # Subnets : public
 resource "aws_subnet" "public" {
   count = length(var.public_sub)
@@ -25,11 +17,30 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_subnet" "private" {
+  count = length(var.private_sub)
+  vpc_id = aws_vpc.terra_vpc.id
+  cidr_block = element(var.private_sub,count.index)
+  # availability_zone = element(var.azs,count.index)
+  # map_private_ip_on_launch = true
+  tags = {
+    Name = "Subnet-${count.index+1}"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "terra_igw" {
+  vpc_id = aws_vpc.terra_vpc.id
+  tags = {
+    Name = "ilya-vpc"
+  }
+}
+
 # Route table: attach Internet Gateway 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.terra_vpc.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "172.31.0.0/24"
     gateway_id = aws_internet_gateway.terra_igw.id
   }
   tags = {
@@ -37,9 +48,50 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+# Internet Gateway for the public subnet
+resource "aws_internet_gateway" "test-igw" {
+  vpc_id = aws_vpc.terra_vpc.id
+}
+
 # Route table association with public subnets
 resource "aws_route_table_association" "a" {
   count = length(var.public_sub)
   subnet_id      = element(aws_subnet.public.*.id,count.index)
   route_table_id = aws_route_table.public_rt.id
+}
+
+# Create a NAT gateway with an Elastic IP for each private subnet to get internet connectivity
+resource "aws_eip" "test-eip" {
+  # count      = length(var.private_sub)
+  vpc        = true
+  depends_on = [aws_internet_gateway.test-igw]
+}
+
+resource "aws_nat_gateway" "test-natgw" {
+  allocation_id = aws_eip.test-eip.id
+  subnet_id     = element(aws_subnet.public.*.id, 0)
+  depends_on    = [aws_internet_gateway.test-igw]
+    tags = {
+      Name = "nat"
+  }
+}
+
+# Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.terra_vpc.id
+
+  route {
+    cidr_block     = "172.31.0.0/24"
+    nat_gateway_id = aws_nat_gateway.test-natgw.id
+  }
+    tags = {
+      Name = "privateRouteTable"
+  }
+}
+
+# Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
+resource "aws_route_table_association" "b" {
+  count          = length(var.private_sub)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private_rt.id
 }
